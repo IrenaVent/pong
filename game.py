7926c79c9ae6eb7ac7f9739a5c4869273fb86418
irena <irena.vent@gmail.com>
2021-08-14 13:11:11 +0200

    actualizaciones última revision

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame as pg
 
-SIZE = (800,600)
+SIZE = (800,600) # we decided no parmetric screen
 
 class Bola():
     def __init__(self, x, y, w, h, color=(255,255,255)):
@@ -26,18 +26,17 @@ class Bola():
             self.derecha = True
 
 
-
-class Game():
+class Game(): # el bucle principal lo convertimos en una clase 
     def __init__(self):
         self.pantalla = pg.display.set_mode((SIZE))
 
     def bucleppal(self):
         bola = Bola (SIZE[0]//2-10, SIZE[1]//2-10, 20, 20)
-        game_over = False
+        game_over = False   
         pg.init()
         while not game_over:
-            
             eventos = pg.event.get()
+            
             for evento in eventos:
                 if evento.type == pg.QUIT:
                     game_over = True
@@ -50,8 +49,9 @@ class Game():
             pg.display.flip() 
 
         pg.quit()
+        
 
-if __name__ == "__name__": # para que no lo ejecute el programa si lo llamamos desde otro archivo  
+if __name__ == "__main__": # para que no lo ejecute el programa si lo llamamos desde otro archivo  
     juego = Game() #instanciar la clase
-    juego.bucleppal()
+    juego.bucleppal() #llamar al método de la calse Game de la instancia juego
 
--- pruebas.py
+++ pruebas.py
@@ -2,7 +2,7 @@ import pygame as pg #le ponemos un alias, para no escribir tanto
 
 pg.init()
 
-pantalla = pg.display.set_mode((800, 600)) # cogemos módulo display de pygame
+pantalla = pg.display.set_mode((800, 600)) # cogemos módulo display de pygame, devuelve una surface, un rectángulo // set_mode(size=(0, 0), flags=0, depth=0, display=0, vsync=0) -> Surface
 
 game_over = False
 
@@ -16,6 +16,6 @@ while not game_over:
 
     pantalla.fill((255, 0, 0))
 
-    pg.display.flip() #hay que hacerlo SIEMPRE, will update the contents of the entire display // updates the whole screen
+    pg.display.flip() #hay que hacerlo SIEMPRE, will update the contents of the entire display // updates the whole screen / último mandato del programa
 
 pg.quit() # método (es una función) para finalizar el juego
